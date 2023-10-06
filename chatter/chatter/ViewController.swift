0934e67e6447335907656aca5621126674b62ac7
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-06 10:59:19 +0900

    ADD: 프로젝트 생성

---
+++ chatter/chatter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatter
+//
+//  Created by 이민규 on 2023/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
