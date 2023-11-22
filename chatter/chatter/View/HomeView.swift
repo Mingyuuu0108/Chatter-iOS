055e43483b0ef3708e654c2df6dece065fe10b8a
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-11-22 14:54:36 +0900

    Add: homeView 완성

--- chatter/chatter/View/HomeView.swift
+++ chatter/chatter/View/HomeView.swift
@@ -15,104 +15,133 @@ struct HomeView: View {
     @State private var isSpeaking = false
     @State private var text: String = ""
     @State private var question: String = ""
-    @State private var enwser: String = ""
+    @State private var answer: String = ""
     
     private let synthesizer = AVSpeechSynthesizer()
     
+    // LifeCycle
     var body: some View {
-        VStack(spacing: 0) {
-            VStack {
-                Button {
-                    
-                } label: {
-                    VStack(spacing: 4) {
-                        Text("이전 질문 / 답변 다시보기")
-                        .font(.system(size: 14, weight: .medium))
-                        .foregroundStyle(Color.white)
-                        
-                        Rectangle()
-                            .fill(Color.white)
-                            .frame(width: 140, height: 0.7)
+        view()
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    withAnimation(.easeInOut) {
+                        showLaunchView = false
                     }
                 }
-                Spacer()
-                
-                Text(question == "" ? "채터에게 질문해보세요!" : enwser == "" ? "답변 중..." : "오늘 오후에 강한 비가 내릴 가능성이 매우 높아요, 우산 꼭 챙겨주세요.")
-                    .font(.system(size: 28, weight: .medium))
-                    .foregroundStyle(Color.white)
-                
-                Spacer()
-                
-                Text("\(question)")
-                    .font(.system(size: 14, weight: .medium))
-                    .foregroundStyle(Color.white)
-                    .padding(.bottom, 20)
             }
-            .frame(height: 240)
-            .frame(maxWidth: .infinity)
-            .background(Color.background)
-            
-            ZStack(alignment: .leading) {
-                if text.count == 0 {
-                    Text("텍스트를 입력해주세요")
-                        .foregroundStyle(Color.subTitle)
+    }
+}
+
+// View
+extension HomeView {
+    
+    @ViewBuilder
+    private func view() -> some View {
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        VStack(spacing: 4) {
+                            Text("이전 질문 / 답변 다시보기")
+                                .font(.system(size: 14, weight: .medium))
+                                .foregroundStyle(Color.white)
+                            
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(width: 140, height: 0.7)
+                        }
+                    }
+                    Spacer()
+                    
+                    Text(question == "" ? "채터에게 질문해보세요!" : answer == "" ? "답변 중..." : "\(answer)")
+                        .font(.system(size: 24, weight: .medium))
+                        .foregroundStyle(Color.white)
+                        .multilineTextAlignment(.leading)
+                        .lineSpacing(4)
+                        .padding(.horizontal, 20)
+                    
+                    Spacer()
+                    
+                    Text("\(question)")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundStyle(Color.white)
+                        .padding(.bottom, 20)
+                }
+                .frame(height: 240)
+                .frame(maxWidth: .infinity, maxHeight: 260)
+                .background(Color.background)
+                
+                ZStack(alignment: .leading) {
+                    if text.count == 0 {
+                        Text("텍스트를 입력해주세요")
+                            .foregroundStyle(Color.subTitle)
+                            .padding(.horizontal, 15)
+                    }
+                    
+                    TextField("", text: $text)
+                        .foregroundStyle(Color.black)
                         .padding(.horizontal, 15)
                 }
+                .frame(height: 40)
+                .background(
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color.white)
+                            .shadow(color: Color.black.opacity(0.3), radius: 4, y: 2)
+                        
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color.clear)
+                            .offset( y: 2)
+                    }
+                )
+                .padding(.horizontal, 30)
+                .padding(.top, 20)
                 
-                TextField("", text: $text)
-                    .foregroundStyle(Color.black)
-                    .padding(.horizontal, 15)
-            }
-            .frame(height: 40)
-            .background(
-                ZStack {
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(Color.white)
-                        .shadow(color: Color.black.opacity(0.3), radius: 4, y: 2)
-                    
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(Color.clear)
-                        .offset( y: 2)
+                Button(action: {
+                    askToGPT()
+                }) {
+                    Text("질문하기")
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(Color.subTitle)
                 }
-            )
-            .padding(.horizontal, 30)
-            .padding(.top, 20)
-            
-            Button(action: {
-                askToGPT()
-            }) {
-                Text("질문하기")
-                    .font(.system(size: 14, weight: .semibold))
-                    .foregroundColor(Color.subTitle)
+                .padding(.top, 20)
+                
+                Spacer()
             }
-            .padding(.top, 20)
-            
-            Spacer()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white)
-        .overlay {
-            if showLaunchView {
-                LaunchView()
-            }
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                withAnimation(.easeInOut) {
-                    showLaunchView = false
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .overlay {
+                if showLaunchView {
+                    LaunchView()
                 }
             }
         }
     }
 }
 
+// Function
 extension HomeView {
     
-    func askToGPT() {
+    private func askToGPT() {
+        stopSpeaking()
+        answer = ""
         question = text
+        AF.request("http://10.80.162.160:8000/question",
+                   method: .post,
+                   parameters: ["question": "\(question) 에 대해 띄어쓰기 포함 해서 80글자 이내로 답변해줘"],
+                   encoding: JSONEncoding.default,
+                   headers: ["Accept": "application/json"]
+        )
+        .responseDecodable(of: String.self) { response in
+            guard let answer = response.value else { return }
+            self.answer = answer
+            speakText(answer)
+        }
     }
     
-    func speakText(_ text: String) {
+    private func speakText(_ text: String) {
         if !isSpeaking {
             let speechUtterance = AVSpeechUtterance(string: text)
             speechUtterance.rate = AVSpeechUtteranceDefaultSpeechRate
@@ -121,7 +150,7 @@ extension HomeView {
         }
     }
     
-    func stopSpeaking() {
+    private func stopSpeaking() {
         if isSpeaking {
             synthesizer.stopSpeaking(at: .immediate)
             isSpeaking = false
